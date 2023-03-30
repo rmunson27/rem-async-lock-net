3b9f487bfcd1125087b361496d2c586d70f99c19
rmunson27 <rmunson2.7@gmail.com>
2023-03-29 21:57:41 -0400

    Added async lock project and all relevant files and testing

---
+++ Rem.Threading.AsyncLock/Rem.Threading.AsyncLock.Test/AsyncLockTest.cs
@@ -0,0 +1,50 @@
+﻿namespace Rem.Threading.Test;
+
+/// <summary>
+/// Tests the <see cref="AsyncLock"/> class.
+/// </summary>
+[TestClass]
+public class AsyncLockTest
+{
+    /// <summary>
+    /// Tests locking and unlocking of the async lock using the return values of the
+    /// <see cref="AsyncLock.Lock(CancellationToken)"/> method.
+    /// </summary>
+    [TestMethod]
+    public void TestSynchronousLocking()
+    {
+        using AsyncLock asyncLock = new();
+
+        using (asyncLock.Lock())
+        {
+            // Can't get the lock within the other lock - the attempt will be canceled after 1 second
+            using CancellationTokenSource cts = new(TimeSpan.FromSeconds(1));
+            Assert.ThrowsException<OperationCanceledException>(() => asyncLock.Lock(cts.Token));
+        }
+
+        using (asyncLock.Lock()) { } // This should allow the lock to be acquired, as it was disposed of
+    }
+
+    /// <summary>
+    /// Asynchronously tests locking and unlocking of the async lock using the return values of the
+    /// <see cref="AsyncLock.LockAsync(CancellationToken)"/> method.
+    /// </summary>
+    /// <returns>A task representing the work.</returns>
+    [TestMethod]
+    public async Task TestAsynchronousLocking()
+    {
+        using AsyncLock asyncLock = new();
+
+        using (await asyncLock.LockAsync().ConfigureAwait(false))
+        {
+            // Can't get the lock within the other lock - the attempt will be canceled after 1 second
+            using CancellationTokenSource cts = new(TimeSpan.FromSeconds(1));
+            await Assert.ThrowsExceptionAsync<OperationCanceledException>(
+                            async () => await asyncLock.LockAsync(cts.Token).ConfigureAwait(false))
+                    .ConfigureAwait(false);
+        }
+
+        // This should allow the lock to be acquired, as it was disposed of
+        using (await asyncLock.LockAsync().ConfigureAwait(false)) { }
+    }
+}
---
+++ Rem.Threading.AsyncLock/Rem.Threading.AsyncLock/AsyncLock.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Threading;
+
+namespace Rem.Threading;
+
+/// <summary>
+/// A lock that can be locked asynchronously.
+/// </summary>
+/// <remarks>
+/// Unlike standard .NET locking, this class can be locked synchronously.
+/// The class includes <see cref="Lock(CancellationToken)"/> and <see cref="LockAsync(CancellationToken)"/> methods,
+/// both of which return handles that can be disposed of to unlock the lock, as well as an explicit
+/// <see cref="Unlock"/> method. This allows an asynchronous lock that can be guaranteed to be released, even if an
+/// exception is thrown.
+/// </remarks>
+public sealed class AsyncLock : IDisposable
+{
+    /// <summary>
+    /// Gets a semaphore that is used to provide the lock.
+    /// </summary>
+    private SemaphoreSlim Semaphore { get; } = new(1);
+
+    /// <summary>
+    /// Locks the <see cref="AsyncLock"/> synchronously.
+    /// </summary>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/> to observe.</param>
+    /// <returns></returns>
+    /// <exception cref="ObjectDisposedException">This instance has already been disposed of.</exception>
+    public Handle Lock(CancellationToken cancellationToken = default)
+    {
+        Semaphore.Wait(cancellationToken);
+        return new Handle(this);
+    }
+
+    /// <summary>
+    /// Locks the <see cref="AsyncLock"/> asynchronously.
+    /// </summary>
+    /// <param name="cancellationToken">A <see cref="CancellationToken"/> to observe.</param>
+    /// <returns>A task representing the work.</returns>
+    /// <exception cref="ObjectDisposedException">This instance has already been disposed of.</exception>
+    public async Task<Handle> LockAsync(CancellationToken cancellationToken = default)
+    {
+        await Semaphore.WaitAsync(cancellationToken).ConfigureAwait(false);
+        return new Handle(this);
+    }
+
+    /// <summary>
+    /// Unlocks the <see cref="AsyncLock"/>.
+    /// </summary>
+    /// <exception cref="ObjectDisposedException">This instance has already been disposed of.</exception>
+    public void Unlock() => Semaphore.Release();
+
+    /// <summary>
+    /// Disposes of all resources used by the <see cref="AsyncLock"/>.
+    /// </summary>
+    public void Dispose() => Semaphore.Dispose();
+
+    /// <summary>
+    /// A disposable handle representing locking a <see cref="AsyncLock"/>.
+    /// </summary>
+    public readonly struct Handle : IDisposable
+    {
+        private AsyncLock Lock { get; }
+
+        internal Handle(AsyncLock Lock) { this.Lock = Lock; }
+
+        /// <summary>
+        /// Releases this handle, unlocking the <see cref="AsyncLock"/> that was locked when creating this instance.
+        /// </summary>
+        /// <exception cref="ObjectDisposedException">
+        /// The <see cref="AsyncLock"/> has already been disposed of.
+        /// </exception>
+        public void Dispose() => Lock?.Unlock();
+    }
+}
